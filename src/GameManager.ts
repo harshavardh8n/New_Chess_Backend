3446ae4683a3b04535d8d2c32e3d9646f35c62af
harshavardh8n <harshavardhangaikwad39@gmail.com>
2024-12-19 15:37:37 +0530

    Changes in logic of disconnection

--- src/GameManager.ts
+++ src/GameManager.ts
@@ -1,72 +1,97 @@
 import { WebSocket } from "ws";
 import { Game } from "./Game";
-import { INIT_GAME, MOVE } from "./messages";
+import { INIT_GAME, MOVE, GAME_ABANDONED } from "./messages";
 
-export class GameManager{
+export class GameManager {
+  private games: Game[];
+  private pendingUser: WebSocket | null;
+  private Users: WebSocket[] = [];
 
-    private games:Game[];
-    private pendingUser:WebSocket | null;
-    private Users:WebSocket[]=[]
+  constructor() {
+    this.games = [];
+    this.pendingUser = null;
+    this.Users = [];
+  }
 
-    constructor(){
-        this.games = [];
-        this.pendingUser = null;
-        this.Users = [];
+  addUser(socket: WebSocket) {
+    this.Users.push(socket);
+    console.log("User connected");
+    this.addHandler(socket);
+
+    console.log("--------------------------------------------------------");
+    console.log("No of users are: " + this.Users.length);
+    console.log("No of current games are: " + this.games.length);
+    console.log("No of pending users are: " + this.pendingUser);
+    console.log("--------------------------------------------------------");
+  }
+
+  removeUser(socket: WebSocket) {
+    this.Users = this.Users.filter((x) => x !== socket);
+
+    // Handle case where the user is in a pending state
+    if (this.pendingUser === socket) {
+      this.pendingUser = null;
     }
 
+    // Check if the user was part of any game
+    const game = this.games.find((game) => game.player1 === socket || game.player2 === socket);
+    if (game) {
+      // Notify the other player
+      const otherPlayer = game.player1 === socket ? game.player2 : game.player1;
+      if (otherPlayer.readyState === WebSocket.OPEN) {
+        otherPlayer.send(
+          JSON.stringify({
+            type: GAME_ABANDONED,
+            payload: { message: "The game has been abandoned by your opponent." },
+          })
+        );
+      }
 
-    addUser(socket:WebSocket){
-        // console.log("printing 1")
-        this.Users.push(socket);
-        console.log("user connected");
-        this.addHandler(socket)
-       
+      // Remove the game from the list
+      this.games = this.games.filter((g) => g !== game);
+      console.log("Game ended due to user disconnection");
     }
+  }
 
-    removeUser(socket:WebSocket){
-        this.Users = this.Users.filter(x=>x!==socket);
-       
-        
-    }
+  private addHandler(socket: WebSocket) {
+    socket.on("message", (message) => {
+      const parsedMessage = JSON.parse(message.toString());
+      console.log("Request received");
+      if (parsedMessage.type === INIT_GAME) {
+        if (this.pendingUser) {
+          const game = new Game(socket, this.pendingUser);
+          this.games.push(game);
+          this.pendingUser = null;
+          game.player1.send(
+            JSON.stringify({
+              type: INIT_GAME,
+              payload: { color: "white" },
+            })
+          );
+          game.player2.send(
+            JSON.stringify({
+              type: INIT_GAME,
+              payload: { color: "black" },
+            })
+          );
+        } else {
+          this.pendingUser = socket;
+        }
+      }
 
+      if (parsedMessage.type === MOVE) {
+        const game = this.games.find((game) => game.player1 === socket || game.player2 === socket);
+        if (game) {
+          console.log("Processing move");
+          game.makeMove(socket, parsedMessage.move);
+        }
+      }
+    });
 
-    private addHandler(socket:WebSocket){
-        socket.on("message",(message)=>{
-            const parsedMessage = JSON.parse(message.toString())
-            console.log("req received")
-            if(parsedMessage.type==INIT_GAME){
-                if(this.pendingUser){
-                    const game = new Game(socket,this.pendingUser)
-                    this.games.push(game);
-                    this.pendingUser = null;
-                    game.player1.send(JSON.stringify({
-                        type:INIT_GAME,
-                        payload:{
-                            color:"white"
-                        }
-                    }))
-                    game.player2.send(JSON.stringify({
-                        type:INIT_GAME,
-                        payload:{
-                            color:"black"
-                        }
-                    }))
-
-                }
-                else{
-                    this.pendingUser = socket;
-                }
-                
-            }
-
-            if(parsedMessage.type===MOVE){
-                const game = this.games.find(game=>game.player1==socket || game.player2==socket)
-                if(game){
-                    console.log("inside move")
-                    game.makeMove(socket,parsedMessage.move);
-
-                }
-            }
-        })
-    }
+    // Handle user disconnection
+    socket.on("close", () => {
+      console.log("User disconnected");
+      this.removeUser(socket);
+    });
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,16 @@ const wss = new WebSocketServer({ port: 8080 });
 const gameManager = new GameManager();
 
 wss.on('connection', function connection(ws) {
+    console.log("Connection")
     gameManager.addUser(ws);
+
 
   ws.on("close",()=>{
     gameManager.removeUser(ws);
     console.log("User disconnexted")
   })
 
-  ws.on("message",()=>{
+  // ws.on("message",()=>{
     
-  })
+  // })
 });
